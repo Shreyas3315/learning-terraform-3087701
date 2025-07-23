94a016b64f443bae38edf0f2cb49b0c305efe7ec
Shreyas Ganesh <103350338+Shreyas3315@users.noreply.github.com>
2025-07-23 23:47:01 +0530

    Initial Commit

--- providers.tf
+++ providers.tf
@@ -1,11 +1,13 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-    }
-  }
-}
-
 provider "aws" {
   region  = "us-west-2"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+
+endpoints {
+    ec2 = "https://1d435ff793dc.ngrok-free.app"
+  }
+
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
 }
