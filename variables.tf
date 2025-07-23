325f802e073f22dc3c3e4618f908acc9533b1af0
Shreyas Ganesh <103350338+Shreyas3315@users.noreply.github.com>
2025-07-23 23:50:26 +0530

    Corrected variables

--- variables.tf
+++ variables.tf
@@ -2,5 +2,9 @@
 #  description = "Type of EC2 instance to provision"
 #  default     = "t3.nano"
 #}
-variable "aws_access_key" {}
-variable "aws_secret_key" {}
+variable "aws_access_key" {
+    default="test"
+}
+variable "aws_secret_key" {
+    default="test"
+}
